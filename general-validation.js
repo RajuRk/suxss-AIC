0ade9996b4fbae5bd66be703b8996addf69ac959
RajuRk <rajurk044@gmail.com>
2023-03-13 15:46:47 +0530

    Commit

---
+++ general-validation.js
@@ -0,0 +1,10 @@
+// const form = document.getElementById("general-form");
+// const name = document.getElementById("general-name");
+// const startupName = document.getElementById("general-startupname");
+// const contact = document.getElementById("general-contact");
+// const mail = document.getElementById("general-mail");
+// const designation = document.getElementById("general-designation");
+
+// function checkLength(input, min, max){
+//   if()
+// }
